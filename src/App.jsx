f083f0b07df9392b031b9497d28ba771d7a94424
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-16 15:16:03 +0200

    added hangman word

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Status from './components/Status'
 import Languages from './components/Languages'
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("react")
+
+  const word = Array.from(currentWord).map((letter, index) => (<div key={index} className="hangman-box"><p>{letter.toUpperCase()}</p></div>))
   return (
     <main>
       <Header/>
       <Status/>
       <Languages/>
+      <section className="hangman-word">{word}</section>
     </main>
   )
 }
