d271a23086ddfc965737f6138c5cd5601ef2db77
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-16 15:34:39 +0200

    added a keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ import Languages from './components/Languages'
 function App() {
   const [currentWord, setCurrentWord] = useState("react")
 
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+  console.log(typeof letters);
+
   const word = Array.from(currentWord).map((letter, index) => (<div key={index} className="hangman-box"><p>{letter.toUpperCase()}</p></div>))
+  const keyboard = alphabet.map((letter, index) => (<div key={index} className="keyboard-box"><p>{letter.toUpperCase()}</p></div>) )
+
   return (
     <main>
       <Header/>
       <Status/>
       <Languages/>
       <section className="hangman-word">{word}</section>
+      <section className="flex-wrapper">{keyboard}</section>
     </main>
   )
 }
--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -5,7 +5,7 @@ const Languages = () => {
     const languageBoxes = languages.map(box => <div className="box-div" key={box.name} style={{backgroundColor: box.backgroundColor}}><p  style={{color: box.color}}className="box-p">{box.name}</p></div>)
 
   return (
-    <section className="languages">
+    <section className="flex-wrapper">
         {languageBoxes}
     </section>
   )
