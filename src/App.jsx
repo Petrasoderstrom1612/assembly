ea9cf139bd00fcaa763baab4ee7756842c49bc2a
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-07-18 12:03:37 +0200

    added clsx object

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,21 @@ function App() {
     ))
   }
   const keyboard = alphabet.map((letter, index) => { 
+    const colorClass = clsx(
+      "keyboard-btn",
+      {
+      green: guessedLetters.includes(letter) && currentWord.includes(letter),
+      red: guessedLetters.includes(letter) && !currentWord.includes(letter),
+      yellow: !guessedLetters.includes(letter), // default
+    })
     return ( 
-    // <button key={index} className={`keyboard-btn ${guessedLetters.includes(letter) && currentWord.includes(letter) ? "green" : guessedLetters.includes(letter) && !currentWord.includes(letter) ? "red" : "yellow"}`} onClick={() => saveGuessedLetter(letter)} >{letter.toUpperCase()}</button>) 
+    //<button key={index} className={`keyboard-btn ${guessedLetters.includes(letter) && currentWord.includes(letter) ? "green" : guessedLetters.includes(letter) && !currentWord.includes(letter) ? "red" : "yellow"}`} onClick={() => saveGuessedLetter(letter)} >{letter.toUpperCase()}</button>) 
+    //<button key={index} className={clsx("keyboard-btn",guessedLetters.includes(letter) && currentWord.includes(letter) && "green", guessedLetters.includes(letter) && !currentWord.includes(letter) && "red", "yellow")} onClick={() => saveGuessedLetter(letter)} >{letter.toUpperCase()}</button>) 
     // If the letter is guessed and is part of the mystery word | if the letter is guessed but is not in the mystery world | if not guessed yet
-    <button key={index} className={`keyboard-btn ${clsx(guessedLetters.includes(letter) && currentWord.includes(letter) && "green" , guessedLetters.includes(letter) && !currentWord.includes(letter) && "red", "yellow")}`} onClick={() => saveGuessedLetter(letter)} >{letter.toUpperCase()}</button>) 
+    <button key={index} className={colorClass} onClick={() => saveGuessedLetter(letter)}>{letter.toUpperCase()}</button>) //important with the onClick here so the component rerenders and color updates!
 
   }) 
-  const word = Array.from(currentWord).map((letter, index) => (<div key={index} className="hangman-box" ><p>{letter.toUpperCase()}</p></div>))
+  const word = Array.from(currentWord).map((letter, index) => (<div key={index} className="hangman-box"><p>{letter.toUpperCase()}</p></div>))
 
 
   console.log(guessedLetters)
